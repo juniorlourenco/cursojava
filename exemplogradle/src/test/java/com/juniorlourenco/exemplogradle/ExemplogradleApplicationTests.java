24c5ffd28c1e045f33fe041eac35e331b5cfacb9
juniorlourenco <56567317+juniorlourenco@users.noreply.github.com>
2020-09-02 00:55:00 -0400

    first program

---
+++ exemplogradle/src/test/java/com/juniorlourenco/exemplogradle/ExemplogradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorlourenco.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplogradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
