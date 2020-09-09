675896499113c8e8192242114072a1574d3c0fb5
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-09 02:41:47 -0400

    Tipos nao primitivos

---
+++ cursojava/src/tipos/naoprimitivos/NaoPrimitivos.java
@@ -0,0 +1,12 @@
+package tipos.naoprimitivos;
+
+class NaoPrimitivos {
+    public static void main(String[] args) {
+        String texto = "Meu texto para apresentação"; //sequencia de caracteres
+        Void v; //Tipo válido
+        Object o = new Object();
+        Number numero = Integer.valueOf(100);
+        numero.toString();
+        
+    }
+}
